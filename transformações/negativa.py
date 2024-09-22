9acb9c05c21421100db66047210b248adbaa7842
João Victor Ribeiro Santos <joaovictoribeirosantos11@gmail.com>
2024-09-21 23:30:53 -0300

    implementação negativa

---
+++ transformações/negativa.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import numpy as np
+import sys
+
+
+def negativa(image):
+    image_array = np.array(image)
+    height, width, bands = image_array.shape[:3]
+    lookup = np.empty(256, dtype=np.uint8)
+    for i in range(256):
+        lookup[i] = 255 - i
+
+    for i in range(height):
+        for j in range(width):
+            for k in range(bands):
+                image_array[i, j, k] = lookup[image_array[i, j, k]]
+    return Image.fromarray(image_array)
+
+
+img_path = sys.argv[1]
+img = Image.open(img_path)
+
+# negative_img = img.point(lambda i: 255 - i)
+negative_img = negativa(img)
+negative_img.show()
